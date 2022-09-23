620e7fa32f565b4ef48d4495031824a92763775b
Sanjana Aher <Sanjana.Aher@einfochips.com>
2022-09-23 22:53:16 +0530

    Added login page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-
+import {useState} from 'react';
+import  LoginForm from "./Page/Dashboard/LoginForm"
+import "./Page/Dashboard/ind.css"
 import './App.css';
 import Dashboard  from './Page/Dashboard/Dashboard';
 
 function App() {
+  const adminUser={
+    email:"admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name:"", email:""});
+  const [error, setError] = useState("");
+
+  const login = details =>{
+    console.log(details);
+    if(details.email === adminUser.email && details.password===adminUser.password){
+      console.log("Logged in");
+      setUser({
+        email: details.email,
+        password:details.password
+      })
+    }else{
+      console.log("Details do not match! ")
+      setError("Details do not match!")
+    }
+  }
+
+  const logout = () => {
+    console.log("Logout")
+    setUser({email:""})
+  }
   return (
     <div className="App">
-      <Dashboard/>
+       {(user.email !== "") ? (
+        <div className='welcome'>
+          <div>
+          <button className='log' onClick={logout}>Logout</button>
+          </div>
+          <Dashboard/>
+         
+        </div>
+      ):(
+        <LoginForm Login={login} error={error}/>
+      )}
+     
     </div>
   );
 }
---
+++ src/Page/Dashboard/LoginForm.js
@@ -0,0 +1,39 @@
+import React,{useState} from 'react'
+import './ind.css'
+import login from'./login.png'
+
+function LoginForm({Login ,error}) {
+    const [details,setDetails] = useState({ email:"", password:""});
+
+    const submitHandler = e => {
+        e.preventDefault();
+        Login(details);
+    }
+  return (
+    <form onSubmit={submitHandler}>
+        <div className='form-inner'>
+            <h2>Login</h2>
+            {(error !== "")? ( <div className='error'>{error}</div>):""}
+            <div className="login">
+            <img src={login} width="300" style={{position: 'relative'}} alt="login"/>
+          </div>
+            <div className='form-group'>
+                <label htmlFor='email'>Email:</label>
+                <input type='email' name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+
+
+            </div>
+            <div className='form-group'>
+                <label htmlFor='passwords'>Password:</label>
+                <input type='password' name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+
+
+            </div>
+            <input className='submit-btn' type="submit" value="LOGIN"/>
+            
+        </div>
+    </form>
+  )
+}
+
+export default LoginForm
